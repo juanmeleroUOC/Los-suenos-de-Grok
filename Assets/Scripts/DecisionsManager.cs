d0748fd4f829ecbf0044f4bb40c0dbbc500dd904
Juan <juanmelero@uoc.edu>
2025-05-10 14:42:36 +0200

    Levels transitions

--- Assets/Scripts/DecisionsManager.cs
+++ Assets/Scripts/DecisionsManager.cs
@@ -1,11 +1,15 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DecisionsManager : MonoBehaviour
 {
     private Dictionary<string, int> takenDecisions = new Dictionary<string, int>();
     public string nextLevel;
+
+    public Image fadeImage;
 
     private void Start()
     {
@@ -23,14 +27,34 @@ public class DecisionsManager : MonoBehaviour
     {
 
         PlayerPrefs.SetInt(decisionID, option);
-        PlayerPrefs.Save();  
-      //  Debug.Log($"Decision saved: {decisionID} -> Option {option}");
-        GetAllDecisions();
-        SceneManager.LoadScene(nextLevel);
-        Time.timeScale = 1f;
+        PlayerPrefs.Save();
+        StartCoroutine(FadeAndLoadScene());
+    }
 
+    private IEnumerator FadeAndLoadScene()
+    {
+        fadeImage.gameObject.SetActive(true);
+      // UIManager.instance.CloseDecisionScreen();
+
+        Color color = fadeImage.color;
+        float duration = 0.5f;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float alpha = Mathf.Clamp01(elapsed / duration);
+            fadeImage.color = new Color(color.r, color.g, color.b, alpha);
+            elapsed += Time.unscaledDeltaTime;
+            yield return null;
+        }
+
+        fadeImage.color = new Color(color.r, color.g, color.b, 1f);
+
+        Time.timeScale = 1f;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        SceneManager.LoadScene(nextLevel);
     }
 
     public int GetDecision(string decisionID)
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class UIManager : MonoBehaviour
 
@@ -16,6 +17,39 @@ public class UIManager : MonoBehaviour
     public Slider musicVolSlider, sfxVolSlider;
 
     public string mainMenu, levelSelect;
+
+    public Image fadeImage;
+
+    private void Start()
+    {
+        Debug.Log("STARTED CANVAS");
+        if (fadeImage != null)
+        {
+            StartCoroutine(FadeFromBlack());
+        }
+    }
+    private IEnumerator FadeFromBlack()
+    {
+        float fadeDuration = 2f;
+
+        fadeImage.gameObject.SetActive(true);
+        Color color = fadeImage.color;
+
+      //yield return new WaitForSeconds(.3f);
+
+        float t = 0;
+
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, t / fadeDuration);
+            fadeImage.color = new Color(color.r, color.g, color.b, alpha);
+            yield return null;
+        }
+
+        fadeImage.color = new Color(color.r, color.g, color.b, 0f);
+        fadeImage.gameObject.SetActive(false);
+    }
 
     private void Awake()
     {
